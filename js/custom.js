a3e8f15978ac760f90a49915007c7df2dafddb7b
Pit <orel-1993@yandex.com>
2019-05-30 16:41:00 +0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -248,12 +248,89 @@ $(function(){
     })
 
 
-    
-
-    
     $('.js-remove-card').on('click', function(){
 		$(this).parents('.row-table__td').fadeOut(500, function(){$(this).remove()});
 		return false;
 	});
-    
+
+
+
+	if($('.anchor-menu').length){
+		 
+	      $('.anchor-menu a').on("click", function(e){
+	          var anchor = $(this);
+	          $('html, body').stop().animate({
+	              scrollTop: $(anchor.attr('href')).offset().top -40
+	          }, 800);
+	          e.preventDefault();
+	      });
+
+
+		  $(".anchor-menu a").click(function(event){
+		    $(".anchor-menu a").removeClass("active");//удаляет все активные элементы
+		    $(this).addClass("active");
+		  });
+
+		  jQuery(window).scroll(function(){
+		    var $sections = $('.b-slide');
+			$sections.each(function(i,el){
+		        var top  = $(el).offset().top-70;
+		        var bottom = top +$(el).height();
+		        var scroll = $(window).scrollTop();
+		        var id = $(el).attr('id');
+		    	if( scroll > top && scroll < bottom){
+		            $('.anchor-menu a').removeClass('active');
+					$('.anchor-menu a[href="#'+id+'"]').addClass('active');
+
+		        }
+		    })
+		 });
+	      
+	};
 });
+
+if($('#aside').length){
+	(function(){
+	var a = document.querySelector('#aside'), b = null, P = 10;
+	window.addEventListener('scroll', Ascroll, false);
+	document.body.addEventListener('scroll', Ascroll, false);
+	function Ascroll() {
+	  if (b == null) {
+	    var Sa = getComputedStyle(a, ''), s = '';
+	    for (var i = 0; i < Sa.length; i++) {
+	      if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+	        s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
+	      }
+	    }
+	    b = document.createElement('div');
+	    b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+	    a.insertBefore(b, a.firstChild);
+	    var l = a.childNodes.length;
+	    for (var i = 1; i < l; i++) {
+	      b.appendChild(a.childNodes[1]);
+	    }
+	    a.style.height = b.getBoundingClientRect().height + 'px';
+	    a.style.padding = '0';
+	    a.style.border = '0';
+	  }
+	  var Ra = a.getBoundingClientRect(),
+	      R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('#content').getBoundingClientRect().bottom);  // селектор блока, при достижении нижнего края которого нужно открепить прилипающий элемент
+	  if ((Ra.top - P) <= 0) {
+	    if ((Ra.top - P) <= R) {
+	      b.className = 'stop';
+	      b.style.top = - R +'px';
+	    } else {
+	      b.className = 'sticky';
+	      b.style.top = P + 'px';
+	    }
+	  } else {
+	    b.className = '';
+	    b.style.top = '';
+	  }
+	  window.addEventListener('resize', function() {
+	    a.children[0].style.width = getComputedStyle(a, '').width
+	  }, false);
+	}
+	})()
+
+}
